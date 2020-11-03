74e8e89e976ab925789ca69e47fc0ea2a106f297
misael <javiermisael8@gmail.com>
2020-11-03 02:11:09 -0800

    Nav Movil con logica mejorada

--- logica.js
+++ logica.js
@@ -1,22 +1,15 @@
-let boton = document.querySelector("#icono");
-let somos=document.querySelector("#somos");
-let estudiantes=document.querySelector("#estudiantes");
-let personal=document.querySelector("#personal");
-let egresados=document.querySelector("#egresados");
-let regreso_somos = document.querySelector("#regresar1")
-let regreso_estudiantes = document.querySelector("#regresar2")
-let regreso_personal = document.querySelector("#regresar3")
-let regreso_egresados = document.querySelector("#regresar4")
-let fondo_negro_somos=document.querySelector("#fondo1")
-let fondo_negro_estudiantes=document.querySelector("#fondo2")
-let fondo_negro_personal=document.querySelector("#fondo3")
-let fondo_negro_egresados=document.querySelector("#fondo4")
-
+const slide = document.querySelector(".encabezado__cuerpo__menu")
+const pantalla_blanca=document.querySelector(".encabezado__cuerpo__contenedor")
+const pantalla_negra=document.querySelector(".encabezado__cuerpo_fondo")
+const lista=document.querySelector(".encabezado__cuerpo__lista")
+const print=console.log
 let menuDesactivado=true;
 let contador = 0;
-
+/*funciones sincronas
+asincronas
+callback hell*/
 //hamburguesa
-boton.addEventListener("click",()=>{
+document.querySelector("#icono").addEventListener("click",()=>{
     let enlaces = document.querySelector("#enlaces");
     let hamburguesa= document.querySelector("#icono");
     if(contador==0){
@@ -25,103 +18,173 @@ boton.addEventListener("click",()=>{
         contador=1
     }else{
         enlaces.className = ('encabezado__enlaces --desactivado')
-        desactivar_ventana("#submenu1","#sub1","#fondo1")
-        desactivar_ventana("#submenu2","#sub2","#fondo2")
-        desactivar_ventana("#submenu3","#sub3","#fondo3")
-        desactivar_ventana("#submenu4","#sub4","#fondo4")
+        desactivar_ventana()
         enableScroll()
         contador = 0
     }
     hamburguesa.classList.toggle("is-active")
 })
-//activa la ventana blanca emergente
-somos.addEventListener("click",()=>{activar_ventana("#submenu1","#sub1","#fondo1")})
-estudiantes.addEventListener("click",()=>{activar_ventana("#submenu2","#sub2","#fondo2")})
-personal.addEventListener("click",()=>{activar_ventana("#submenu3","#sub3","#fondo3")})
-egresados.addEventListener("click",()=>{activar_ventana("#submenu4","#sub4","#fondo4")})
+
+const enlace_menu=Array.apply(null,document.querySelectorAll(".link"))
+enlace_menu.map(i=>i.addEventListener('click',function(e){
+    activar_ventana(i.lastChild.innerText,i.id)
+}))
 
 //desactiva ventana blanca emergente
-regreso_somos.addEventListener("click",()=>{desactivar_ventana("#submenu1","#sub1","#fondo1")})
-regreso_estudiantes.addEventListener("click",()=>{desactivar_ventana("#submenu2","#sub2","#fondo2")})
-regreso_personal.addEventListener("click",()=>{desactivar_ventana("#submenu3","#sub3","#fondo3")})
-regreso_egresados.addEventListener("click",()=>{desactivar_ventana("#submenu4","#sub4","#fondo4")})
-fondo_negro_somos.addEventListener("click",()=>{desactivar_ventana("#submenu1","#sub1","#fondo1")})
-fondo_negro_estudiantes.addEventListener("click",()=>{desactivar_ventana("#submenu2","#sub2","#fondo2")})
-fondo_negro_personal.addEventListener("click",()=>{desactivar_ventana("#submenu3","#sub3","#fondo3")})
-fondo_negro_egresados.addEventListener("click",()=>{desactivar_ventana("#submenu4","#sub4","#fondo4")})
+document.querySelector(".encabezado__cuerpo__icono").addEventListener("click",()=>{desactivar_ventana()})
+pantalla_negra.addEventListener("click",()=>{desactivar_ventana()})
+
 //desactivar ventana con slide
-let slide_somos = document.querySelector("#menu1")
-let slide_estudiantes = document.querySelector("#menu2")
-let slide_personal = document.querySelector("#menu3")
-let slide_egresados = document.querySelector("#menu4")
-let inicio_slide1;
-slide_somos.addEventListener('touchstart', function (event) {toque_inicial(event,'sub1')})
-slide_somos.addEventListener('touchmove', function(event){toque_movimiento(event,'sub1')});
-slide_somos.addEventListener('touchend', function (event) {toque_final(event,"#submenu1","#sub1","#fondo1")})
-slide_estudiantes.addEventListener('touchstart', function (event) {toque_inicial(event,'sub2')})
-slide_estudiantes.addEventListener('touchmove', function(event){toque_movimiento(event,'sub2')});
-slide_estudiantes.addEventListener('touchend', function (event) {toque_final(event,"#submenu2","#sub2","#fondo2")})
-slide_personal.addEventListener('touchstart', function (event) {toque_inicial(event,'sub3')})
-slide_personal.addEventListener('touchmove', function(event){toque_movimiento(event,'sub3')});
-slide_personal.addEventListener('touchend', function (event) {toque_final(event,"#submenu3","#sub3","#fondo3")})
-slide_egresados.addEventListener('touchstart', function (event) {toque_inicial(event,'sub4')})
-slide_egresados.addEventListener('touchmove', function(event){toque_movimiento(event,'sub4')});
-slide_egresados.addEventListener('touchend', function (event) {toque_final(event,"#submenu4","#sub4","#fondo4")})
-
-function toque_inicial(event,cuerpo_blanco) {
+slide.addEventListener('touchstart', (event)=>{
     inicio_slide1=event.touches[0].clientY 
-    document.getElementById(cuerpo_blanco).style.transition='none'
-}
-
-function toque_movimiento(event,cuerpo_blanco) {
+    pantalla_blanca.style.transition='none'
+})
+slide.addEventListener('touchmove', (event)=>{
     let touch = event.touches[0]; 
     let cambio = touch.clientY-inicio_slide1
     if(cambio<0)return  
     cambio=cambio+50
-    document.getElementById(cuerpo_blanco).style.top = cambio +'px';
+    pantalla_blanca.style.top = cambio +'px';
     event.preventDefault()
-}
-
-function toque_final(event,contenido,ventana,fondo) {
+})
+slide.addEventListener('touchend', (event)=>{
     let cambio = event.changedTouches[0].clientY-inicio_slide1
     let parte = screen.height/3
     if(cambio<parte){
-        document.querySelector(ventana).style.transition='all .3s'
-        document.querySelector(ventana).style.top='12%'
+        pantalla_blanca.style.transition='all .3s'
+        pantalla_blanca.style.top='12%'
     }else{
-        desactivar_ventana(contenido,ventana,fondo)
-    }
-}
+        desactivar_ventana()
+    }})
 
-function activar_ventana(id_contenido,id_ventana,id_fondo) {
-    let cuerpo=document.querySelector(id_contenido)
-    let cuerpo_blanco=document.querySelector(id_ventana)
-    let fondo_negro=document.querySelector(id_fondo)
-    cuerpo.style.visibility="visible"
-    fondo_negro.style.visibility="visible"
-    fondo_negro.style.background="rgba(0,0,0,.3)"
+const items=[
+    {
+        name:"Quienes somos",
+        id:"somos",
+        elementos:[
+            {
+                icono:"fa-door-open",
+                titulo:"Bienvenida"
+            },{
+                icono:"fa-address-book",
+                titulo:"Directorio"
+            },{
+                icono:"fa-calendar-alt",
+                titulo:"Calendario"
+            },{
+                icono:"fa-users",
+                titulo:"Transparencia"
+            },{
+                icono:"fa-search-location",
+                titulo:"Oferta Educativa"
+            },{
+                icono:"fa-building",
+                titulo:"Departamentos"
+            }
+        
+        ]
+    },{
+        name:"Estudiantes",
+        id:"estudiantes",
+        elementos:[
+            {
+                icono:"fa-door-open",
+                titulo:"Mind Box"
+            },{
+                icono:"fa-address-book",
+                titulo:"Moodle"
+            },{
+                icono:"fa-calendar-alt",
+                titulo:"Servicios Escolares"
+            },{
+                icono:"fa-users",
+                titulo:"Residencia"
+            },{
+                icono:"fa-search-location",
+                titulo:"Oferta Educativa"
+            },{
+                icono:"fa-building",
+                titulo:"Encuentra Empleo"
+            },{
+                icono:"fa-building",
+                titulo:"Créditos Comp"
+            }
+        
+        ]
+    },{
+        name:"Personal ITM",
+        id:"personal",
+        elementos:[
+            {
+                icono:"fa-door-open",
+                titulo:"Mind Box"
+            },{
+                icono:"fa-address-book",
+                titulo:"Moodle"
+            },{
+                icono:"fa-calendar-alt",
+                titulo:"Correos"
+            },{
+                icono:"fa-users",
+                titulo:"Recursos Humanos"
+            },{
+                icono:"fa-search-location",
+                titulo:"Centro de Inf"
+            },{
+                icono:"fa-building",
+                titulo:"Departamentos"
+            }
+        
+        ]
+    },{
+        name:"Egresados",
+        id:"egresados",
+        elementos:[
+            {
+                icono:"fa-door-open",
+                titulo:"Servicios Escolares"
+            },{
+                icono:"fa-address-book",
+                titulo:"Cedula Profesional"
+            }
+        ]
+    },
+]
+
+function activar_ventana(encabezado,id) {
+    document.querySelector(".encabezado__cuerpo").style.visibility="visible"
+    pantalla_negra.style.visibility="visible"
+    pantalla_blanca.style.top="12vh"
     menuDesactivado=false
-    cuerpo_blanco.style.top="12vh"
+    document.querySelector(".encabezado__cuerpo__titulo").innerText=encabezado
+    Generar_items(id)
 }
 
-function desactivar_ventana(id_contenido,id_ventana,id_fondo) {
-    let cuerpo=document.querySelector(id_contenido)
-    let cuerpo_blanco=document.querySelector(id_ventana)
-    let fondo_negro=document.querySelector(id_fondo)
-    cuerpo.style.visibility="hidden"
-    fondo_negro.style.visibility="hidden"
-    cuerpo_blanco.style.top="100%"
+function Generar_items(id) {
+    const lista=document.querySelector(".encabezado__cuerpo__lista")
+    let contador=0
+    items.filter((x,y)=>{if(x.id==id)contador=y})
+    items[contador].elementos.forEach((indice)=>{
+        const item=document.createElement('li')
+        item.classList='encabezado__cuerpo__item'
+        const item_icono=document.createElement('i')
+        item_icono.classList='encabezado__cuerpo__itemicono fas '+indice.icono
+        const item_cuerpo=document.createElement('p')
+        item_cuerpo.classList='encabezado__cuerpo__txt'
+        item_cuerpo.innerHTML='<p>'+indice.titulo+'</p>'
+        item.appendChild(item_icono)
+        item.appendChild(item_cuerpo)
+        lista.appendChild(item)
+    })
 }
 
+function desactivar_ventana() {
+    document.querySelector(".encabezado__cuerpo").style.visibility="hidden"
+    pantalla_negra.style.visibility="hidden"
+    pantalla_blanca.style.top="100%"
+    lista.innerHTML=''
+}
 
-window.addEventListener('resize', ()=>{
-    if(screen.width > 750){
-        contador=0;
-        enlaces.classList.remove('--activado');
-        enlaces.className = ('encabezado__enlaces --desactivado');
-
-    }
-})
 
 function disableScroll(){  
     var x = window.scrollX;
